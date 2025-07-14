64e97fb453e77663bea96f5ecf7dab3ed8ca93a2
00zkript <hg305718@gmail.com>
2025-07-13 22:56:12 -0500

    feat(ui): add PrimeReact and theme configuration
    - Add PrimeReact library and its theme dependencies
    - Configure theme variables and imports in index.css
    - Update main.tsx to include PrimeReactProvider
    - Add required dependencies including sass-embedded

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { BrowserRouter } from 'react-router';
+// import Tailwind from 'primereact/passthrough/tailwind';
+import './index.css';
+import { PrimeReactProvider } from 'primereact/api';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <StrictMode>
+        <BrowserRouter>
+            {/* <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}> */}
+            <PrimeReactProvider>
+                <App />
+            </PrimeReactProvider>
+        </BrowserRouter>
+    </StrictMode>
+);
